39bf1b1e257813c2125b8b44a1d715d1551830bc
Sunny Jha <sunnyjha11897@gmail.com>
2020-09-29 19:46:06 +0530

    [SUNNY] Add. UseCase3 for checking the options

--- SnakeLadder/Program.cs
+++ SnakeLadder/Program.cs
@@ -4,20 +4,48 @@ namespace SnakeLadder
 {
     class Program
     {
+        //Constants
+        public const int NO_OF_PLAYERS = 1;
+        public const int IS_LADDER = 1;
+        public const int IS_SNAKE = 2;
+
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome To Snake Ladder Problem!");
-            
-            //Constants
-            int NO_OF_PLAYERS = 1;
-
+           
             //Variables
-            int startPosition = 0;
+            int position = 0;
 
             Random random = new Random();
 
             int die = random.Next(1, 7);
             Console.WriteLine("Rolling: " + die);
+
+            //Checking 
+            int optionPlay = random.Next(0, 3);
+            switch (optionPlay)
+            {
+                case IS_LADDER:
+                    position += die;
+                    Console.WriteLine("IS_LADDER");
+                    break;
+                case IS_SNAKE:
+                    if (position == 0)
+                    {
+                        position = 0;
+                        Console.WriteLine("IS_SNAKE");
+                    }
+                    else
+                    {
+                        position -= die;
+                        Console.WriteLine("IS_SNAKE");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("NO_PLAY");
+                    break;
+            }
+            Console.WriteLine("NEW_POSITION: " + position);
         }
     }
 }
